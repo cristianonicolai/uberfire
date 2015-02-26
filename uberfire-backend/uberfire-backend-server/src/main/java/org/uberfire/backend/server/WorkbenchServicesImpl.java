ef686ac3ed2ca2aa1e904e18d55a95e69afaf61c
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2015-02-26 10:24:50 -0500

    BZ-1192459: additional sync cleanup in order to avoid deadlocks

--- uberfire-backend/uberfire-backend-server/src/main/java/org/uberfire/backend/server/WorkbenchServicesImpl.java
+++ uberfire-backend/uberfire-backend-server/src/main/java/org/uberfire/backend/server/WorkbenchServicesImpl.java
@@ -16,10 +16,7 @@
 package org.uberfire.backend.server;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -27,29 +24,18 @@ import javax.inject.Named;
 import com.thoughtworks.xstream.XStream;
 import org.jboss.errai.bus.server.annotations.Service;
 import org.uberfire.io.IOService;
-import org.uberfire.java.nio.IOException;
-import org.uberfire.java.nio.file.FileVisitResult;
-import org.uberfire.java.nio.file.Files;
 import org.uberfire.java.nio.file.NoSuchFileException;
 import org.uberfire.java.nio.file.Path;
-import org.uberfire.java.nio.file.SimpleFileVisitor;
 import org.uberfire.java.nio.file.StandardDeleteOption;
-import org.uberfire.java.nio.file.attribute.BasicFileAttributes;
-import org.uberfire.workbench.model.PanelDefinition;
 import org.uberfire.workbench.model.PerspectiveDefinition;
 import org.uberfire.workbench.model.SplashScreenFilter;
 import org.uberfire.workbench.services.WorkbenchServices;
-
-import static org.uberfire.commons.validation.PortablePreconditions.*;
-import static org.uberfire.java.nio.file.Files.*;
 
 @Service
 @ApplicationScoped
 public class WorkbenchServicesImpl
         implements
         WorkbenchServices {
-
-    public static final String PERSPECTIVE_EXTENSION = ".perspective";
 
     @Inject
     @Named("configIO")
@@ -61,11 +47,11 @@ public class WorkbenchServicesImpl
     private final XStream xs = new XStream();
 
     @Override
-    public synchronized void save( final String perspectiveId,
-                                   final PerspectiveDefinition perspective ) {
+    public void save( final String perspectiveId,
+                      final PerspectiveDefinition perspective ) {
         final String xml = xs.toXML( perspective );
         final Path perspectivePath = userServices.buildPath( "perspectives",
-                                                             perspectiveId + PERSPECTIVE_EXTENSION );
+                                                             perspectiveId + ".perspective" );
         try {
             ioService.startBatch( perspectivePath.getFileSystem() );
             ioService.write( perspectivePath, xml );
@@ -75,8 +61,8 @@ public class WorkbenchServicesImpl
     }
 
     @Override
-    public synchronized void save( SplashScreenFilter splashFilter ) {
-        final String xml = xs.toXML(splashFilter);
+    public void save( SplashScreenFilter splashFilter ) {
+        final String xml = xs.toXML( splashFilter );
         final Path splashFilterPath = userServices.buildPath( "splash",
                                                               splashFilter.getName() + ".filter" );
         try {
@@ -89,8 +75,8 @@ public class WorkbenchServicesImpl
 
     @Override
     public PerspectiveDefinition loadPerspective( final String perspectiveName ) {
-        final Path perspectivePath = userServices.buildPath("perspectives",
-                perspectiveName + PERSPECTIVE_EXTENSION );
+        final Path perspectivePath = userServices.buildPath( "perspectives",
+                                                             perspectiveName + ".perspective" );
         if ( ioService.exists( perspectivePath ) ) {
             final String xml = ioService.readAllString( perspectivePath );
             return (PerspectiveDefinition) xs.fromXML( xml );
@@ -100,42 +86,7 @@ public class WorkbenchServicesImpl
     }
 
     @Override
-    public Set<PerspectiveDefinition> loadPerspectives() {
-        final Set<PerspectiveDefinition> result = new HashSet<PerspectiveDefinition>();
-        final Path perspectivesPath = userServices.buildPath( "perspectives" );
-        if ( ioService.exists( perspectivesPath ) ) {
-
-            walkFileTree(perspectivesPath, new SimpleFileVisitor<Path>() {
-                public FileVisitResult visitFile( final Path file, final BasicFileAttributes attrs ) throws IOException {
-                    try {
-                        checkNotNull("file", file);
-                        checkNotNull("attrs", attrs);
-                        String fileName = file.getFileName().toString();
-                        if ( fileName.endsWith(PERSPECTIVE_EXTENSION) && attrs.isRegularFile() ) {
-                            String perspectiveName = fileName.substring(0, fileName.indexOf(PERSPECTIVE_EXTENSION));
-                            PerspectiveDefinition def = loadPerspective( perspectiveName );
-                            if (def != null) result.add( def );
-                        }
-                    } catch (final Exception ex) {
-                        return FileVisitResult.TERMINATE;
-                    }
-                    return FileVisitResult.CONTINUE;
-                }
-            });
-        }
-        return result;
-    }
-
-    @Override
-    public synchronized void removePerspectiveState( final String perspectiveId ) {
-        final Path perspectivePath = userServices.buildPath( "perspectives", perspectiveId + ".perspective");
-        if ( ioService.exists( perspectivePath ) ) {
-            ioService.delete( perspectivePath );
-        }
-    }
-
-    @Override
-    public synchronized void removePerspectiveStates() {
+    public void removePerspectiveStates() {
         final Path perspectivesPath = userServices.buildPath( "perspectives" );
         if ( ioService.exists( perspectivesPath ) ) {
             try {
@@ -184,7 +135,7 @@ public class WorkbenchServicesImpl
     }
 
     @Override
-    public synchronized void saveDefaultEditors( final Map<String, String> properties ) {
+    public void saveDefaultEditors( final Map<String, String> properties ) {
         final StringBuilder text = new StringBuilder();
         for ( String key : properties.keySet() ) {
             text.append( String.format( "%s=%s", key, properties.get( key ) ) );
