42e7ec713589356182b8451630bdcc3de089760b
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2015-04-27 15:35:01 +1000

    Menu PatternFly review

--- uberfire-workbench/uberfire-workbench-client-views-patternfly/src/main/java/org/uberfire/client/views/pfly/menu/UserMenu.java
+++ uberfire-workbench/uberfire-workbench-client-views-patternfly/src/main/java/org/uberfire/client/views/pfly/menu/UserMenu.java
@@ -1,84 +1,66 @@
 package org.uberfire.client.views.pfly.menu;
 
-import javax.annotation.PostConstruct;
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-
-import org.gwtbootstrap3.client.ui.AnchorButton;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import org.gwtbootstrap3.client.ui.AnchorListItem;
 import org.gwtbootstrap3.client.ui.DropDownMenu;
-import org.gwtbootstrap3.client.ui.ListItem;
-import org.gwtbootstrap3.client.ui.constants.ButtonType;
 import org.gwtbootstrap3.client.ui.constants.Styles;
 import org.gwtbootstrap3.client.ui.constants.Toggle;
 import org.gwtbootstrap3.client.ui.html.Span;
 import org.gwtbootstrap3.client.ui.html.Text;
-import org.jboss.errai.ioc.client.container.IOC;
+import org.jboss.errai.common.client.api.Caller;
 import org.jboss.errai.security.shared.api.identity.User;
 import org.jboss.errai.security.shared.api.identity.User.StandardUserProperties;
-import org.uberfire.workbench.model.menu.MenuFactory.CustomMenuBuilder;
-import org.uberfire.workbench.model.menu.MenuItem;
-import org.uberfire.workbench.model.menu.MenuVisitor;
-import org.uberfire.workbench.model.menu.impl.BaseMenuCustom;
+import org.jboss.errai.security.shared.service.AuthenticationService;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 /**
  * Provides the menu that appears in the top right corner of the screen. Shows the current user's name and provides a
  * menu that allows logging out.
  */
 @ApplicationScoped
-public class UserMenu extends ListItem {
+public class UserMenu extends UtilityMenu {
 
-
-    public class Builder implements CustomMenuBuilder {
-
-        @Override
-        public void push( CustomMenuBuilder element ) {
-            throw new UnsupportedOperationException( "Not implemented." );
-        }
-
-        @Override
-        public MenuItem build() {
-            return new BaseMenuCustom<UserMenu>() {
-
-                @Override
-                public UserMenu build() {
-                    return IOC.getBeanManager().lookupBean( UserMenu.class ).getInstance();
-                }
-
-                @Override
-                public void accept( MenuVisitor visitor ) {
-                    visitor.visit( this );
-                }
-            };
-        }
-
-    }
 
     @Inject
     private User user;
+
+    @Inject
+    private Caller<AuthenticationService> authService;
 
     private final DropDownMenu menu = new DropDownMenu();
 
     @PostConstruct
     private void setup() {
-        AnchorButton mainContent = new AnchorButton( ButtonType.DEFAULT );
+        anchor.addStyleName(Styles.DROPDOWN_TOGGLE);
+        anchor.setDataToggle(Toggle.DROPDOWN);
+        addStyleName(Styles.DROPDOWN_TOGGLE);
 
-        // we want AnchorButton for its magic dropdown Caret feature, not its buttony styling
-        mainContent.removeStyleName( Styles.BTN );
-        mainContent.removeStyleName( ButtonType.DEFAULT.getCssName() );
+        final Span userIcon = new Span();
+        userIcon.addStyleName("pficon");
+        userIcon.addStyleName("pficon-user");
 
-        Span userIcon = new Span();
-        userIcon.addStyleName( "pficon" );
-        userIcon.addStyleName( "pficon-user" );
+        anchor.add(userIcon);
+        anchor.add(new Text(formattedUsername()));
+        final Span caret = new Span();
+        caret.addStyleName(Styles.CARET);
+        anchor.add(caret);
 
-        mainContent.add( userIcon );
-        mainContent.add( new Text( formattedUsername() ) );
+        addStyleName(Styles.DROPDOWN);
+        add(anchor);
 
-        // this has to be done last because it causes the dropdown caret to be appended to the content.
-        mainContent.setDataToggle( Toggle.DROPDOWN );
-
-        addStyleName( Styles.DROPDOWN );
-        add( mainContent );
-        add( menu );
+        final AnchorListItem logout = new AnchorListItem("Logout");
+        logout.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                logout();
+            }
+        });
+        menu.add(logout);
+        add(menu);
     }
 
     /**
@@ -100,7 +82,7 @@ public class UserMenu extends ListItem {
      * Tries to return the user's first and/or last names. If neither is available, returns the user's ID instead.
      */
     private String formattedUsername() {
-        StringBuilder sb = new StringBuilder();
+        final StringBuilder sb = new StringBuilder();
         if ( user.getProperty( StandardUserProperties.FIRST_NAME ) != null ) {
             sb.append( user.getProperty( StandardUserProperties.FIRST_NAME ) );
         }
@@ -112,5 +94,12 @@ public class UserMenu extends ListItem {
             sb.append( user.getIdentifier() );
         }
         return sb.toString();
+    }
+
+    /**
+     * Logout user
+     */
+    public void logout() {
+        authService.call().logout();
     }
 }
