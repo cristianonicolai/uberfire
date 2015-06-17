e01c15adcb24a8c6b5b50c030ae94a3dde9357cd
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2015-06-17 21:36:52 +1000

    Menu update

--- uberfire-workbench/uberfire-workbench-client-views-patternfly/src/main/java/org/uberfire/client/views/pfly/menu/WorkbenchMenuBarView.java
+++ uberfire-workbench/uberfire-workbench-client-views-patternfly/src/main/java/org/uberfire/client/views/pfly/menu/WorkbenchMenuBarView.java
@@ -15,22 +15,35 @@
  */
 package org.uberfire.client.views.pfly.menu;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
-import org.gwtbootstrap3.client.ui.*;
+import org.gwtbootstrap3.client.ui.Anchor;
+import org.gwtbootstrap3.client.ui.Button;
+import org.gwtbootstrap3.client.ui.DropDown;
+import org.gwtbootstrap3.client.ui.DropDownMenu;
+import org.gwtbootstrap3.client.ui.Icon;
+import org.gwtbootstrap3.client.ui.Navbar;
+import org.gwtbootstrap3.client.ui.NavbarBrand;
+import org.gwtbootstrap3.client.ui.NavbarCollapse;
+import org.gwtbootstrap3.client.ui.NavbarHeader;
+import org.gwtbootstrap3.client.ui.NavbarNav;
 import org.gwtbootstrap3.client.ui.base.AbstractListItem;
+import org.gwtbootstrap3.client.ui.constants.IconType;
 import org.gwtbootstrap3.client.ui.constants.Styles;
+import org.gwtbootstrap3.client.ui.constants.Toggle;
+import org.gwtbootstrap3.client.ui.html.Span;
+import org.gwtbootstrap3.client.ui.html.Text;
 import org.jboss.errai.ioc.client.container.IOCResolutionException;
 import org.jboss.errai.security.shared.api.identity.User;
 import org.uberfire.client.workbench.widgets.menu.WorkbenchMenuBarPresenter;
 import org.uberfire.security.authz.AuthorizationManager;
 import org.uberfire.workbench.model.menu.MenuPosition;
 import org.uberfire.workbench.model.menu.Menus;
-
-import javax.annotation.PostConstruct;
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Instance;
-import javax.inject.Inject;
 
 /**
  * The Menu Bar widget
@@ -49,6 +62,12 @@ public class WorkbenchMenuBarView extends Composite implements WorkbenchMenuBarP
 
     private final NavbarNav primaryNavBar = new NavbarNav();
 
+    private final NavbarNav navBar = new NavbarNav();
+
+    private final DropDown menuItemDropDown = new DropDown();
+
+    private final DropDownMenu menuItemDropDownMenu = new DropDownMenu();
+
     @Inject
     private UtilityNavbar utilityNavbar;
 
@@ -57,24 +76,49 @@ public class WorkbenchMenuBarView extends Composite implements WorkbenchMenuBarP
         final Navbar root = new Navbar();
         root.addStyleName( "navbar-pf" );
 
+        final NavbarHeader headerContainer = new NavbarHeader();
         try {
-            final NavbarHeader headerContainer = new NavbarHeader();
             final NavbarBrand brand = new NavbarBrand();
             brand.add(menuBarBrand.get());
             headerContainer.add(brand);
-            root.add( headerContainer );
+            final Span divider = new Span();
+            divider.addStyleName( "divider-vertical" );
+            headerContainer.add( divider );
         } catch ( IOCResolutionException e ) {
             // app didn't provide a branded header bean
         }
+        root.add( headerContainer );
 
-        NavbarCollapse collapsibleContainer = new NavbarCollapse();
+        primaryNavBar.addStyleName( "navbar-primary" );
+        primaryNavBar.addStyleName( "uf-menu-compact" );
 
-        primaryNavBar.addStyleName("navbar-primary");
-
+        final NavbarCollapse collapsibleContainer = new NavbarCollapse();
         collapsibleContainer.add( utilityNavbar );
+//        collapsibleContainer.add( navBar );
         collapsibleContainer.add( primaryNavBar );
 
         root.add( collapsibleContainer );
+
+        final Button btnToggle = new Button();
+        btnToggle.removeStyleName( "btn-default" );
+        btnToggle.addStyleName( Styles.NAVBAR_TOGGLE );
+        btnToggle.setDataToggle( Toggle.COLLAPSE );
+        btnToggle.setDataTargetWidget( collapsibleContainer );
+        final Icon icon = new Icon( IconType.BARS );
+        icon.addStyleName( "fa-inverse" );
+        btnToggle.add( icon );
+        headerContainer.add( btnToggle );
+
+        final Anchor anchor = new Anchor();
+        anchor.add( new Text( "Menus" ) );
+        final Span caret = new Span();
+        caret.addStyleName( Styles.CARET );
+        anchor.add( caret );
+        anchor.setDataToggle( Toggle.DROPDOWN );
+        anchor.setDataTargetWidget( menuItemDropDown );
+        menuItemDropDown.add( anchor );
+        menuItemDropDown.add( menuItemDropDownMenu );
+        navBar.add( menuItemDropDown );
 
         initWidget( root );
     }
@@ -105,6 +149,7 @@ public class WorkbenchMenuBarView extends Composite implements WorkbenchMenuBarP
                     switch (position) {
                         case LEFT:
                             primaryNavBar.add(menuContent);
+//                            menuItemDropDownMenu.add( menuContent );
                             break;
                         case CENTER:
                             primaryNavBar.insert(menuContent, primaryNavBar.getWidgetCount() - 1);
@@ -123,6 +168,14 @@ public class WorkbenchMenuBarView extends Composite implements WorkbenchMenuBarP
     public void clear() {
         primaryNavBar.clear();
         utilityNavbar.clear();
+    }
+
+    public void expand(){
+        primaryNavBar.removeStyleName( "hide" );
+    }
+
+    public void collapse(){
+        primaryNavBar.addStyleName( "hide" );
     }
 
 }
